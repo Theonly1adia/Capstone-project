ae3c2dc618a9b4745926c8c3b20613be113f18d7
Adia Holt <adia.holt@gmail.com>
2024-07-20 10:31:06 -0500

    update files

--- addToCart.js
+++ addToCart.js
@@ -1,22 +1,42 @@
 
-var addItemId = 0;
-function addToCart(item){
-    addItemId += 1;
-    var selectedItem = document.createElement('div');
-    selectedItem.classList.add('cartImg');
-    selectedItem.setAttribute('id',addItemId);
-    var img = document.createElement('img');
-    img.setAttribute('src',item.children[0].currentSrc);
-    var title = document.createElement('div');
-    title.innerText = item.children[1].innerText;
-    var label = document.createElement('div');
-    label.innerText = item.children[2].children[0].innerText;
-    var select = document.createElement('span');
-    select.innerText = item.children[2].children[1].value;
-    label.append(select);
-    var cartItems = document.getElementById('title');
-    selectedItem.append(img);
-    selectedItem.append(title);
-    selectedItem.append(label);
-    cartItems.append(selectedItem);
+
+
+
+
+let cart = [];
+let total = 0;
+
+function addToCart(product, price) {
+    cart.push({ product, price });
+    total += price;
+    displayCart();
+}
+
+function removeFromCart(index) {
+    total -= cart[index].price;
+    cart.splice(index, 1);
+    displayCart();
+}
+
+function clearCart() {
+    cart = [];
+    total = 0;
+    displayCart();
+}
+
+function displayCart() {
+    const cartContainer = document.getElementById('cart-items');
+    cartContainer.innerHTML = '';
+    
+    cart.forEach((item, index) => {
+        const cartItem = document.createElement('div');
+        cartItem.className = 'cart-item';
+        cartItem.innerHTML = `
+            <p>${item.product} - $${item.price}</p>
+            <button onclick="removeFromCart(${index})">Remove</button>
+        `;
+        cartContainer.appendChild(cartItem);
+    });
+    
+    document.getElementById('total').innerText = total;
 }
